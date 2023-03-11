83b75ff9bdda4fb04633cd3129c5df3c10b782e3
Amolo <railamolo@gmail.com>
2023-03-11 09:49:12 +0300

    support payment updates

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -96,7 +96,7 @@ router.get("/:id", [requireJWT], async (req: Request, res: Response) => {
                     id: userId
                 }
             });
-            let post = await db.post.findMany({
+            let post = await db.media.findMany({
                 where: { id: id, userId: userId }
             });
             if (post.length > 0) {
@@ -104,17 +104,43 @@ router.get("/:id", [requireJWT], async (req: Request, res: Response) => {
                     post: {
                         description: post[0].description,
                         createdBy: post[0].userId,
-                        image: post[0].image,
-                        imageUrl: `${req.protocol + "://" + req.get('host') + "/files/" + post[0].image}`,
                         updatedAt: post[0].updatedAt,
                         createdAt: post[0].createdAt,
                         id: post[0].id
+
                     },
                     status: "success"
                 });
                 return;
             }
             res.status(401).json({ error: "Post not found", status: "error" });
+            return;
+
+        }
+    } catch (error) {
+        console.log(error)
+        res.statusCode = 400;
+        res.json({ status: "error", error: error });
+        return;
+    }
+});
+
+// Get Single Media Post.
+router.delete("/:id", [requireJWT], async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        let token = req.headers.authorization || '';
+        let decodedSession = decodeSession(process.env['SECRET_KEY'] as string, token.split(' ')[1])
+        if (decodedSession.type == 'valid') {
+            let userId = decodedSession.session.userId;
+            let user = await db.user.findUnique({
+                where: {
+                    id: userId
+                }
+            });
+            let post = await db.media.delete({where: { id: id, }});
+
+            res.status(200).json({ message: "Deleted media", status: "success" });
             return;
 
         }
